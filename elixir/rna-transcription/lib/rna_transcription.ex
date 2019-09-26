554bcd4b1df6f97bddec42782a9db42a64d21b27
aleferreiranogueira <ale.ferreiranogueira@gmail.com>
2019-09-26 15:27:35 -0300

    Improve code by using codepointsd

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -10,14 +10,11 @@ defmodule RnaTranscription do
 
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    List.to_string(dna)
-    |> String.split("", trim: true)
-    |> Enum.map(fn
-      "G" -> 'C'
-      "C" -> 'G'
-      "T" -> 'A'
-      "A" -> 'U'
+    Enum.map(dna, fn
+      ?G -> ?C
+      ?C -> ?G
+      ?T -> ?A
+      ?A -> ?U
     end)
-    |> Enum.reduce(fn rna, acc -> acc ++ rna end)
   end
 end
